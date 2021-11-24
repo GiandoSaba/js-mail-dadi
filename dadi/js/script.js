20c825d02ff5500a8da87de7adf4bb4a8a1eda52
GiandoSaba <giandosaba997@gmail.com>
2021-11-24 16:13:47 +0100

    bonus_dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,16 +1,47 @@
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+const input = document.getElementById('userName');
+const button = document.getElementById('btn-start');
+const playerScore = document.querySelector('.playerscore');
+const ComputerScore = document.querySelector('.computerscore');
 
-const dadoPlayer = Math.floor(Math.random() * 6) + 1;
-console.log('Player: ', dadoPlayer);
-const dadoComputer = Math.floor(Math.random() * 6) + 1;
-console.log('Computer: ', dadoComputer);
+let counter = 0;
+let scorePlayer = 0;
+let scoreComputer = 0;
 
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+button.addEventListener('click', 
+function(){
+    counter++;
+    const userName = input.value;
+    const playerName = document.querySelector('.playerscore h2');
+    const computerName = document.querySelector('.computerscore h2');
+    
+    // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+    const dadoPlayer = Math.floor(Math.random() * 6) + 1;
+    const dadoComputer = Math.floor(Math.random() * 6) + 1;        
+    
+    // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+    if (dadoPlayer > dadoComputer) {
+        scorePlayer++;
+    } else if (dadoPlayer == dadoComputer) {
+        console.log('Pareggio')
+    } else {
+        scoreComputer++;
+    }
+    
+    const playerText = `Round ${counter}: ${dadoPlayer}`;
+    const computerText = `Round ${counter}: ${dadoComputer}`;
+    
+    playerName.innerHTML = userName + ': ' + scorePlayer;
+    computerName.innerHTML = 'Computer: ' + scoreComputer;
 
-if (dadoPlayer > dadoComputer) {
-    console.log('Player Win');
-} else if (dadoPlayer == dadoComputer) {
-    console.log('Pareggio');
-} else {
-    console.log('Computer Win')
-}
+    const elementPlayer = document.createElement('p');
+    const elementComputer = document.createElement('p');
+
+    elementPlayer.innerHTML += playerText;
+    elementComputer.innerHTML += computerText;
+
+    playerScore.append(elementPlayer);
+    ComputerScore.append(elementComputer);
+});
+
+
+
